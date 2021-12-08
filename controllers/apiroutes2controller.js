6e00627068f4fd3448cfc370d732492efd69eeb9
nicholasthomson <nalfredthomson@gmail.com>
2021-12-08 15:01:36 -0500

    fixed Songs variables and SongWriters variables

--- controllers/apiroutes2controller.js
+++ controllers/apiroutes2controller.js
@@ -7,10 +7,10 @@ const data = {
   postAlbum:   `INSERT INTO album (album_name, album_release_date, record_label_id, album_id)
                 VALUES (:album_name, :album_release_date, :record_label_id, :album_id);`,
 
-  getSongs: `SELECT song_writer_id, song_writer_first_name, song_writer_last_name, song_writer_birth_date
+  getSongs: `SELECT song_id, track_name, track_duration, album_id, explicit
                 FROM songs;`,
 
-  getSongWriters: `SELECT song_id, track_name, track_duration, album_id, explicit
+  getSongWriters: `SELECT song_writer_id, song_writer_first_name, song_writer_last_name, song_writer_birth_date
                     FROM songwriters;`,
 
   getPerformers: `SELECT artist_id, artist_first_name, artist_last_name, country_of_origin, gender, birth_date
